0edfb7edeeb623b284bc49adad0cb32af00963e7
Martin Lowe <martinlowe673@gmail.com>
2024-03-09 18:56:19 +0000

    title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 
 async function fetchBlogs(){
-  const res = await fetch("http://localhost:3000/api/blog",{next:{
+  const res = await fetch("http://localhost:3001/api/blog",{next:{
     revalidate:10,
   },
 
@@ -15,8 +15,10 @@ export default async function Home() {
   const posts = await fetchBlogs();
   console.log(posts)
   return (
- <main>
-  hi
+ <main className ="w-full h-full">
+<div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-4 rounded-lg bg-slate-800 drop-shadow-xl">
+  <h1 className="text-slate-200 text-center text-2xl font-extrabold font-[verdana]">Full Stack Blog </h1>
+</div>
  </main>
   );
 }
